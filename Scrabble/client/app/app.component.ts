97c4d9acc8ba493fd3b386163dd5d5f4fd2cf2b3
David Thanh  Vo <david-thanh.vo@polymtl.ca>
2017-01-22 15:46:30 -0500

    - Added the 15x15 board

--- Scrabble/client/app/app.component.ts
+++ Scrabble/client/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <grid></grid>
+  `,
 })
 export class AppComponent { name = 'Angular'; }
