4e79e915019644d4f3c225d40d4b3d09d0d3a4ac
David <david_vo@hotmail.ca>
2017-01-26 22:03:23 -0500

    DTV: - Removed Scrabble grid # 1, keeping grid #2 - Added implementation of chat room **CLIENT SIDE ONLY - Slight style change on the easel

--- Scrabble/client/app/scrabbleBoard.component.ts
+++ Scrabble/client/app/scrabbleBoard.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'scrabble-main-board', 
     templateUrl: 'app/scrabble.html',
-    styleUrls: ['app/scrabble.css']
+    styleUrls: ['app/scrabbleBoard.css']
 })
 
-export class ScrabbleBoard {
+export class ScrabbleBoardComponent {
 
     columnNumberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
@@ -17,23 +17,23 @@ export class ScrabbleBoard {
         'MD': 'scrabbleImages/doubleWordScore.jpg',
         'LT': 'scrabbleImages/tripleLetterScore.jpg',
         'MT': 'scrabbleImages/tripleWordScore.jpg'
-    }
+    };
 
-    line1 = ['MT', 'N', 'N', 'LD', 'N', 'N', 'N', 'MT', 'N', 'N', 'N', 'LD', 'N', 'N', 'MT']
-    line2 = ['N', 'MD', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'MD', 'N']
-    line3 = ['N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N']
-    line4 = ['LD', 'N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N', 'LD']
-    line5 = ['N', 'N', 'N', 'N', 'MD', 'N', 'N', 'N', 'N', 'N', 'MD', 'N', 'N', 'N', 'N']
-    line6 = ['N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N']
-    line7 = ['N', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'N']
-    line8 = ['MT', 'N', 'N', 'LD', 'N', 'N', 'N', 'S', 'N', 'N', 'N', 'LD', 'N', 'N', 'MT']
-    line9 = ['N', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'N']
-    line10 = ['N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N']
-    line11 = ['N', 'N', 'N', 'N', 'MD', 'N', 'N', 'N', 'N', 'N', 'MD', 'N', 'N', 'LT', 'N']
-    line12 = ['LD', 'N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N', 'LD']
-    line13 = ['N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N']
-    line14 = ['N', 'MD', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'MD', 'N']
-    line15 = ['MT', 'N', 'N', 'LD', 'N', 'N', 'N', 'MT', 'N', 'N', 'N', 'LD', 'N', 'N', 'MT']
+    line1 = ['MT', 'N', 'N', 'LD', 'N', 'N', 'N', 'MT', 'N', 'N', 'N', 'LD', 'N', 'N', 'MT'];
+    line2 = ['N', 'MD', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'MD', 'N'];
+    line3 = ['N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N'];
+    line4 = ['LD', 'N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N', 'LD'];
+    line5 = ['N', 'N', 'N', 'N', 'MD', 'N', 'N', 'N', 'N', 'N', 'MD', 'N', 'N', 'N', 'N'];
+    line6 = ['N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N'];
+    line7 = ['N', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'N'];
+    line8 = ['MT', 'N', 'N', 'LD', 'N', 'N', 'N', 'S', 'N', 'N', 'N', 'LD', 'N', 'N', 'MT'];
+    line9 = ['N', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'LD', 'N', 'N'];
+    line10 = ['N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N'];
+    line11 = ['N', 'N', 'N', 'N', 'MD', 'N', 'N', 'N', 'N', 'N', 'MD', 'N', 'N', 'LT', 'N'];
+    line12 = ['LD', 'N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N', 'LD'];
+    line13 = ['N', 'N', 'MD', 'N', 'N', 'N', 'LD', 'N', 'LD', 'N', 'N', 'N', 'MD', 'N', 'N'];
+    line14 = ['N', 'MD', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'LT', 'N', 'N', 'N', 'MD', 'N'];
+    line15 = ['MT', 'N', 'N', 'LD', 'N', 'N', 'N', 'MT', 'N', 'N', 'N', 'LD', 'N', 'N', 'MT'];
 
     line = {
         0:this.line1,
