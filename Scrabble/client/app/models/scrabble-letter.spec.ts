f0c01ccf5ee2522174f57724b456ab3552da845f
David Thanh  Vo <david-thanh.vo@polymtl.ca>
2017-02-01 19:01:32 -0500

    DTV: - Added unit test for Scrabble Letter - Added unit test for EaselGeneratorService

---
+++ Scrabble/client/app/models/scrabble-letter.spec.ts
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import { ScrabbleLetter } from "./scrabble-letter";
+let letter : ScrabbleLetter;
+
+describe("ScrabbleLetter letter validation", () => {
+
+    beforeEach(() => {
+        letter = new ScrabbleLetter();
+    });
+
+    it("should be an instance of ScrabbleLetter", () => {
+        expect(letter).to.be.an.instanceof(ScrabbleLetter);
+    });
+
+    it("should be a string", () => {
+        expect(letter.letter).to.be.a('string');
+    });
+
+    it("should should be a letter between A and Z", () => {
+        expect(letter.letter.charCodeAt(0)).to.be.above(64).and.to.be.below(91);
+    });
+
+    it("should contain the .jpg extension", () => {
+        expect(letter.imageSource).to.contain(".jpg");
+    });
+
+});
+
+
--- Scrabble/client/app/services/socketService.ts
+++ Scrabble/client/app/services/socketService.ts
@@ -1,20 +1,18 @@
 import { Injectable } from "@angular/core";
 import * as io from "socket.io-client";
-//import { SocketCanalNames } from "../../../server/app/SocketCanalNames";
 
-const CONNECTION = "connection";
-const DISCONNECTION = "disconnect";
 //Message sent by the client when he wants to play a game
 const NEW_GAME_DEMAND = "newGameDemand";
 //Message sent by the server if the information sent are not valid
 const INVALID_DEMAND = "invalidDemand";
 //Message sent by the server if the name already exists
 const NAME_OR_SOCKET_ALREADY_EXISTS = "errorNameExists";
-//Message sent by the server if the name previously sended by the client is invalid
+//Message sent by the server if the name previously received by the client is invalid
 const INVALID_NAME = "errorInvalidName";
 //Message sent by the server when the number of missing players to begin the game has changed
 const PLAYERS_MISSING = "playersMissing";
-const portNumber = 3002;
+
+const PORT_NUMBER = 3002;
 
 
 @Injectable()
@@ -23,7 +21,7 @@ export class SocketService {
     socket: SocketIOClient.Socket;
 
     constructor () {
-        this.socket = io.connect("http://localhost:" + String(portNumber));
+        this.socket = io.connect("http://localhost:" + String(PORT_NUMBER));
     }
 
     public sendNewDemandRequest(playerName: string, gameType: string, responseFunctions: Function[]) {
