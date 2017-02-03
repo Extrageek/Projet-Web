a26f38fe4ac0bbf163a9d3363f0f1389ae0fc19f
Mathieu KABORE <matdonell@gmail.com>
2017-02-02 23:58:45 -0500

    fixed

--- Sudoku/server/app/services/puzzle-manager.service.spec.ts
+++ Sudoku/server/app/services/puzzle-manager.service.spec.ts
@@ -7,10 +7,7 @@ import * as puzzleManagerService from './puzzle-manager.service';
 
 describe('Puzzle Manager Service', () => {
     it('should initialize a grid', () => {
-        let puzzleManager: PuzzleManager = new puzzleManagerService.PuzzleManager();
-        let puzzle1: Puzzle = new Puzzle();
+      
             // TODO: Must be completed
        });
-       
-    });
 });
