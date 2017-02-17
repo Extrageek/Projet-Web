344093a4c6c5b626baa1474c8a10fdc6784c879a
Extrageek <loccer16@hotmail.com>
2017-02-17 16:35:53 -0500

    LOC: - Changed the order of enumeration of stoneColor for better understanding.

--- Curling/client/app/models/stone.ts
+++ Curling/client/app/models/stone.ts
@@ -2,8 +2,8 @@ import { ObjectLoader, Group, MeshPhongMaterial, Object3D, Sphere, Vector3 } fro
 import { GameComponent } from "./gameComponent.interface";
 
 export enum StoneColor {
-    Red = 0,
-    Blue = 1,
+    Blue = 0,
+    Red = 1,
     NumberOfColors = 2
 }
 
