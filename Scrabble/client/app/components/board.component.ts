28b07c493b77b2b5119a0c5c1e0cf75216ec9dad
Rami Eid <Rami Eid>
2017-02-22 17:06:50 -0500

    RE + MK : - Board Service was disposable

--- Scrabble/client/app/components/board.component.ts
+++ Scrabble/client/app/components/board.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
-import { BoardService } from "../services/board.service";
 import { Square } from '../models/square/square';
 import { Board } from '../models/board/board';
 
 @Component({
     moduleId: module.id,
-    providers: [BoardService],
+    providers: [Board],
     selector: "scrabble-main-board-selector",
     templateUrl: "../../assets/templates/scrabble.html",
     styleUrls: ["../../assets/stylesheets/scrabble-board.css"]
@@ -14,7 +13,7 @@ import { Board } from '../models/board/board';
 export class ScrabbleBoardComponent {
     scrabbleGrid: Board;
 
-    constructor(scrabbleBoardService: BoardService) {
-        this.scrabbleGrid = scrabbleBoardService.board;
+    constructor(scrabbleBoard: Board) {
+        this.scrabbleGrid = new Board();
     }
 }
