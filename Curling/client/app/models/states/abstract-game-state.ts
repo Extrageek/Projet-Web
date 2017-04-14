e06b0bf2463eda2524dca0f15b16ebcb51fb6eb9
Rami Eid <rami.eid@polymtl.ca>
2017-04-14 09:30:50 -0400

    RE : - abstract-game error fixed

--- Curling/client/app/models/states/abstract-game-state.ts
+++ Curling/client/app/models/states/abstract-game-state.ts
@@ -1,8 +1,7 @@
 import { Vector3 } from "three";
 import { StoneSpin } from "../stone";
 import { IGameInfo } from "../../services/game-handler/game-info.interface";
-import { IGameServices } from ".
-./../services/game-handler/games-services.interface";
+import { IGameServices } from "../../services/game-handler/games-services.interface";
 import { IShotParameters } from "../shot-parameters.interface";
 import { IGameState } from "../game-state.interface";
 
