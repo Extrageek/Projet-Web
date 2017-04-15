e448fe6a3181cdbf63da9f6a72899cb18a8dfe18
David-Thanh Vo <david-thanh.vo@polymtl.ca>
2017-04-15 17:47:09 -0400

    DTV:  - Fixed sound-manager tests

--- Curling/client/app/services/spec/sound-manager.spec.ts
+++ Curling/client/app/services/spec/sound-manager.spec.ts
@@ -1,45 +1,45 @@
 import { SoundManager } from "../sound-manager";
-import { Object3D } from "three";
+import { Object3D, Audio, AudioListener } from "three";
 
 import { expect } from "chai";
 
-let instance: SoundManager;
+let instance = SoundManager.getInstance();
 describe("Sound manager should should", function () {
     this.timeout(15000);
-
-    before(() => {
-        instance = SoundManager.getInstance();
-    });
 
     it("Initialize the instance of a sound manager, should not ", () => {
         expect(instance).to.not.equal(null);
     });
 
-    it("Broom sound should be correctly initialized and playable", () => {
+    it("Broom sound should be correctly initialized and playable", (done) => {
+
         setTimeout(() => {
             let broomSoundIn = instance.playBroomInSound();
             expect(broomSoundIn).to.be.an.instanceof(Audio);
             expect(broomSoundIn.getLoop()).to.equal(false);
+            done();
         }, 2000);
     });
 
-    it("Initialize the instance of a sound manager should not be undefined", () => {
+    it("Broom sound should be correctly initialized and playable", (done) => {
         setTimeout(() => {
             let broomSoundOut = instance.playBroomOutSound();
             expect(broomSoundOut).to.be.an.instanceof(Audio);
             expect(broomSoundOut.getLoop()).to.equal(false);
+            done();
         }, 2000);
     });
 
-    it("Initialize the instance of a sound manager should not be undefined", () => {
+    it("Collision sound should be correctly initialized and playable", (done) => {
         setTimeout(() => {
             let collisionSound = instance.playCollisionSound();
             expect(collisionSound).to.be.an.instanceof(Audio);
             expect(collisionSound.getLoop()).to.equal(false);
+            done();
         }, 2000);
     });
 
-    it("Initialize the instance of a sound manager should not be undefined", () => {
+    it("Initialize the instance of a sound manager should not be undefined", (done) => {
 
         let obj: Object3D;
         obj = new Object3D();
@@ -48,6 +48,7 @@ describe("Sound manager should should", function () {
             expect(audioListener).to.be.an.instanceof(AudioListener);
             obj.add(audioListener);
             expect(obj.children.length).to.equal(1);
+            done();
         }, 2000);
     });
 });
