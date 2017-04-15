9c93bbf9dba696f80bd2fa760715761b5d8fa60f
David-Thanh Vo <david-thanh.vo@polymtl.ca>
2017-04-15 12:01:35 -0400

    DTV:  - Fixed bug where computer AI would not change after quitting game

--- Curling/client/app/models/states/starting.ts
+++ Curling/client/app/models/states/starting.ts
@@ -2,6 +2,8 @@ import { AbstractGameState } from "./abstract-game-state";
 import { LoadingStone } from "./loading-stone";
 import { IGameServices } from "../../services/game-handler/games-services.interface";
 import { IGameInfo } from "../../services/game-handler/game-info.interface";
+import { ComputerTurn } from "../../models/states/computer-turn";
+import { StatesHandler } from "../../services/game-handler/states-handler";
 
 /**
  * This state is used to initialize the game. It resets all the services.
@@ -30,6 +32,9 @@ export class Starting extends AbstractGameState {
         this._gameServices.stoneHandler.cleanAllStones();
         this._gameInfo.gameStatus.resetGameStatus();
         this._gameInfo.gameStatus.randomFirstPlayer();
+        let computerAI = StatesHandler.getInstance().createComputerAI(this._gameServices.userService.difficulty,
+            this._gameInfo.rink);
+        (<ComputerTurn> ComputerTurn.getInstance()).computerAI = computerAI;
         this.leaveState(LoadingStone.getInstance());
     }
 }
