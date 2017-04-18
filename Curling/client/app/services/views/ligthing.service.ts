ad515617d6f025799fa3115e6eac574639350728
Rami Eid <rami.eid@polymtl.ca>
2017-04-18 11:39:40 -0400

    RE : - added spotlights at the end of the rink

--- Curling/client/app/services/views/ligthing.service.ts
+++ Curling/client/app/services/views/ligthing.service.ts
@@ -52,19 +52,19 @@ export class LightingService {
         spotlight3.position.set(19, 10, 12);
         spotlight3.target.position.set(0, 0, 8);
 
-        // let spotlightHouseFar = new SpotLight(THREE.ColorKeywords.white, 0.8, 0, 0.4);
-        // spotlightHouseFar.penumbra = 0.34;
-        // spotlightHouseFar.position.set(-9, 10, 17);
-        // spotlightHouseFar.target.position.set(0, 0, 17);
+        let spotlightHouseFar = new SpotLight(THREE.ColorKeywords.white, 0.8, 0, 0.4);
+        spotlightHouseFar.penumbra = 0.34;
+        spotlightHouseFar.position.set(-9, 10, 17);
+        spotlightHouseFar.target.position.set(0, 0, 17);
 
-        // let spotlight4 = new SpotLight(THREE.ColorKeywords.white, 0.6, 0, 0.3);
-        // spotlight4.penumbra = 0.8;
-        // spotlight4.position.set(9, 10, 12);
-        // spotlight4.target.position.set(0, 0, 23);
+        let spotlight4 = new SpotLight(THREE.ColorKeywords.white, 0.6, 0, 0.3);
+        spotlight4.penumbra = 0.8;
+        spotlight4.position.set(9, 10, 12);
+        spotlight4.target.position.set(0, 0, 23);
 
         this._spotLights.push(
             spotlightHouseNear, spotlight1,
-            spotlight2, spotlight3, /*spotlightHouseFar, spotlight4*/);
+            spotlight2, spotlight3, spotlightHouseFar, spotlight4);
     }
 
     public setUpLighting(scene: Scene) {
@@ -78,5 +78,4 @@ export class LightingService {
         scene.add(spotLight.target);
         scene.add(spotLight);
     }
-
 }
