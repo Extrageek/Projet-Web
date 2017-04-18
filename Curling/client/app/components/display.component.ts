2d1877c516703e9151bc4a354262ecddc1de85c0
Rami Eid <rami.eid@polymtl.ca>
2017-04-18 11:15:42 -0400

    RE : - start new game redirect to difficulty, not game

--- Curling/client/app/components/display.component.ts
+++ Curling/client/app/components/display.component.ts
@@ -18,6 +18,7 @@ import { RenderService } from "../services/game-handler/render.service";
         "../../assets/stylesheets/leaderboard-component.css"
     ]
 })
+
 export class DisplayComponent implements OnInit {
     _userSettingService: UserService;
     _computerName: string;
@@ -129,7 +130,7 @@ export class DisplayComponent implements OnInit {
     public startNewGame() {
         this.gameStatusService.resetGameStatus();
         this.renderService.stopGame().then(() => {
-            this.router.navigate(["/game"]);
+            this.router.navigate(["/difficulty"]);
             this.renderService.initAndStart();
         });
     }
