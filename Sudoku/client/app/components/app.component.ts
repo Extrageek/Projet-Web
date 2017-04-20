81fa1d8886ddd4fc578aa7a5de6356fbcb63ba0f
David-Thanh Vo <david-thanh.vo@polymtl.ca>
2017-04-19 21:27:05 -0400

    DTV: - Padding on logo

--- Sudoku/client/app/components/app.component.ts
+++ Sudoku/client/app/components/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "sudoku-app",
-    templateUrl: "../assets/templates/app.component.html"
+    templateUrl: "../assets/templates/app.component.html",
+    styleUrls: [ "../../assets/stylesheets/app.component.css" ],
 })
 
 export class AppComponent {
